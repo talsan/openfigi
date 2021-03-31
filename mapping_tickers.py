b61d73b49b6752f6ba2e4e5b3360dccc87781127
datadoodling <tsansani@gmail.com>
2021-03-31 15:55:02 -0700

    build two approaches (ticker/exchange mapping vs name search)

---
+++ mapping_tickers.py
@@ -0,0 +1,77 @@
+import requests
+import config
+import json
+import pandas as pd
+import re
+
+pd.set_option('max_rows', 500)
+pd.set_option('max_columns', 1000)
+
+openfigi_apikey = config.api_key
+xls = pd.ExcelFile('./tci_names/Focused Quality Holdings as of 3_23_2021.xlsx')
+fq = pd.read_excel(xls, 'ACWI-ex_US')
+
+acwi = pd.read_csv('./tci_names/Acwi_today_editted.csv',keep_default_na=False)
+# edits = NA TSE --> NA
+
+# map exchanges
+# https://stockmarketmba.com/globalstockexchanges.php
+acwi['Financial Instrument.1'] = acwi['Financial Instrument.1'].str.replace('\.', '',regex=True)
+acwi['exchange_ib'] = acwi['Financial Instrument.1'].str.extract('\s([A-Z0-9]*)$')
+acwi['ticker_local'] = acwi['Financial Instrument.1'].str.extract('^([A-Z0-9]*)\s?')
+exchange_map = {
+    'IBIS': 'GY',  # e.g. xetra(ibis) maps to GY bloomberg exchange id
+    'SEHK': 'HK',
+    'FWB': 'GF',
+    'FWB2': 'GF',
+    'SBF': 'FP',
+    'AEB': 'NA',
+    'LSE': 'LN',
+    'TSE': 'JT',
+    'SFB': 'SS',
+    'SWB2': 'GS',
+    'CN': 'CN'}
+acwi['exchange_bb'] = acwi['exchange_ib'].apply(lambda x: exchange_map.get(x)).fillna('US')
+
+fq_w_info = fq.merge(acwi, how='left', left_on='Description', right_on='Company Name')
+fq_w_info = fq_w_info.dropna(subset=['ticker_local'])
+
+jobs = []
+for i, row in fq_w_info.iterrows():
+    if re.search('(\s|-)(ADR|GDR)', row['Company Name']):
+        sec_type = 'Depositary Receipt'
+    else:
+        sec_type = 'Common Stock'
+
+    job = {'idType': 'TICKER',
+           'idValue': row['ticker_local'],
+           'exchCode': row['exchange_bb'],
+           'securityType2': sec_type,
+           'marketSecDes': 'Equity'}
+    jobs.append(job)
+
+response = requests.post(url='https://api.openfigi.com/v2/mapping',
+                         headers={'Content-Type': 'application/json',
+                                  'X-OPENFIGI-APIKEY': openfigi_apikey},
+                         data=json.dumps(jobs))
+
+id_maps = pd.DataFrame()
+for i,r in enumerate(response.json()):
+    if r.get('error') is None:
+        id_map = pd.json_normalize(r, 'data')
+        id_map.insert(0,'job_id',i)
+        id_maps = id_maps.append(id_map)
+    else:
+        print(i)
+        print(jobs[i])
+
+jobs_df = pd.DataFrame(jobs).reset_index().rename(columns={'index':'job_id'})
+
+# merge back to input
+id_map_all = jobs_df.merge(id_maps, how='left', on='job_id')
+
+# assert no misses
+assert(id_map_all['compositeFIGI'].isna().sum()==0)
+
+# assert no duplicates
+assert(id_map_all.groupby('job_id').size().max()==1)
---
+++ search_names.py
@@ -0,0 +1,93 @@
+import requests
+import config
+import json
+import pandas as pd
+import time
+import re
+
+pd.set_option('max_rows', 500)
+pd.set_option('max_columns', 1000)
+'''
+See https://www.openfigi.com/api for more information.
+'''
+
+openfigi_apikey = config.api_key
+xls = pd.ExcelFile('./tci_names/Focused Quality Holdings as of 3_23_2021.xlsx')
+fq = pd.read_excel(xls, 'ACWI-ex_US')
+acwi = pd.read_csv('./tci_names/Acwi_today.csv')
+
+# map exchanges
+# https://stockmarketmba.com/globalstockexchanges.php
+acwi['exchange_ib'] = acwi['Financial Instrument.1'].str.extract('\s([A-Z].*)$')
+exchange_map = {
+    'IBIS': 'GY',  # e.g. xetra(ibis) maps to GY bloomberg exchange id
+    'SEHK': 'HK',
+    'FWB': 'GF',
+    'FWB2': 'GF',
+    'SBF': 'FP',
+    'AEB': 'NA',
+    'LSE': 'LN',
+    'TSE': 'JT',
+    'SFB': 'SS',
+    'SWB2': 'GS'}
+acwi['exchange_bb'] = acwi['exchange_ib'].apply(lambda x: exchange_map.get(x)).fillna('US')
+
+fq_w_info = fq.merge(acwi, how='left', left_on='Description', right_on='Company Name')
+fq_w_info = fq_w_info.dropna(subset=['Company Name'])
+
+jobs = []
+for i, row in fq_w_info.iterrows():
+    if re.search('(\s|-)(ADR|GDR)',row['Company Name']):
+        sec_type = 'Depositary Receipt'
+    else:
+        sec_type = 'Common Stock'
+
+    job = {'job_id': i,
+           'payload': {
+               'query': row['Company Name'],
+              #'currency': row['Trading Currency'],
+               'exchCode': row['exchange_bb'],
+               'securityType2': sec_type,
+               'marketSecDes': 'Equity'}}
+    jobs.append(job)
+
+def request_figi(job_payload, sleep_time=65):
+    response = requests.post(url='https://api.openfigi.com/v2/search',
+                             headers={'Content-Type': 'application/json',
+                                      'X-OPENFIGI-APIKEY': openfigi_apikey},
+                             data=json.dumps(job_payload))
+    retry_count = 0
+    while response.status_code == 429:
+        retry_count += 1
+        print(f'retry number {retry_count}; waiting {sleep_time} seconds before next request...')
+        time.sleep(sleep_time)
+        response = request_figi(job_payload)
+
+    return response
+
+jobs_df = pd.DataFrame()
+id_maps = pd.DataFrame()
+for i, job in enumerate(jobs):
+    print(job)
+
+    job_df = pd.DataFrame(job['payload'],index=[i])
+    job_df.insert(0, 'job_id', job['job_id'])
+    jobs_df = jobs_df.append(job_df)
+
+    response = request_figi(job['payload'])
+    id_map = pd.json_normalize(response.json()['data'])
+    id_map.insert(0, 'job_id', job['job_id'])
+    id_maps = id_maps.append(id_map, ignore_index=True)
+
+# merge back to input
+id_map_all = jobs_df.merge(id_maps,how='left',on='job_id')
+
+#
+
+
+
+# deal with duplicates
+
+id_maps.groupby('job_id').size().max()
+
+print(json.dumps(response.json(), indent=4))
